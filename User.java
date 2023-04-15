0d9ceafd5b2b176f68fe36bc9b6ad58c6fdb3941
Muhammad Abubakar Siddique <73362079+Abubakar4101@users.noreply.github.com>
2023-04-15 19:10:23 +0500

    Add files via upload

---
+++ User.java
@@ -0,0 +1,65 @@
+
+public abstract class User {
+	
+	//Define constructors
+	User() {}
+//	
+	
+	private String firstName;
+	private String lastName;
+	
+	private String username;
+	private String password;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setFirstName(String fn) {
+		this.firstName = fn;
+	}
+	
+	public void setLastName(String ln) {
+		this.lastName = ln;
+	}
+	
+	public void setUsername(String usn) {
+		this.username = usn;
+	}
+	public void setPassword(String psw) {
+		this.password = psw;
+	}
+
+	public void firstName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void lastName() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void username() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void password() {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
