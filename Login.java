f2534090b7fa3d08889dd0cb15ef45edf0f2423e
Sohaib Zahid <72217713+sohaibzahid197@users.noreply.github.com>
2023-05-25 11:30:23 +0500

    Update Login.java
    Added multi-line comments that describe the purpose of Login.java as the main class for the course management system.
    
    It also includes a single-line comment for the firstName method, lastName method, username method, and password method, explaining its purpose in handling user login functionality.

--- Login.java
+++ Login.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 
+/**
+ * The Login class represents a user login functionality. It extends the User class,
+ * which contains methods to collect user information such as first name, last name,
+ * username, and password.
+ */
+
+
 public class Login extends User{
 	public static void askUser() {
 		
 	}
 	
-	
+	 /**
+     * Prompts the user to enter their first name.
+     */
 	@Override
 	public void firstName() {
 		Scanner input = new Scanner(System.in);
@@ -14,32 +23,33 @@ public class Login extends User{
 		
 	}
 
+    /**
+     * Prompts the user to enter their last name.
+     */
 	@Override
 	public void lastName() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Last Name: ");
 		String lastName = input.nextLine();
 	}
-
+  /**
+     * Prompts the user to enter their username.
+     */
 	@Override
 	public void username() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Username: ");
 		String username = input.nextLine();
 	}
-
+  /**
+     * Prompts the user to enter their password.
+     */
 	@Override
 	public void password() {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Password: ");
 		String password = input.nextLine();
 	}
-
-	
-	
-	
-	
-	
 
 	
 }
